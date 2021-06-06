6c2f12023f735c0afbcbe9a0911ff24b30a7f0ea
Quentin Burney <Quentin.burney@gmail.com>
2021-06-06 19:53:27 -0400

    Initial commit

---
+++ src/main/java/org/example/Retire.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+
+import static java.lang.Integer.*;
+
+public class Retire {
+    public static void main(String[] args){
+        int theage, rage, getout=0;
+        System.out.println("What is your current age");
+        Scanner scanner = new Scanner(System.in);
+        String age = scanner.next();
+        System.out.println("At what age would you like to retire?");
+        Scanner scanner1 = new Scanner(System.in);
+        String retireAge = scanner1.next();
+
+            theage = parseInt(age);
+            rage = parseInt(retireAge);
+            getout = rage - theage;
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int retireyear = year + getout;
+       System.out.println("You have " + getout + " years until you can retire.");
+       System.out.println("It is " + year + " so you can retire in " + retireyear);
+    }
+}
